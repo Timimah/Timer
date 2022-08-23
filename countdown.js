161834db308e81ccf14c0e9472448d74c187c561
Timimah <timimah0117@gmail.com>
2022-08-23 14:15:34 +0100

    countdown timer completed

--- countdown.js
+++ countdown.js
@@ -1,15 +1,21 @@
+seconds.disabled = false
+minutes.disabled = false
+hours.disabled = false
+
 hours.focus()
 let t
 let start = () =>{
-    seconds.disabled = 
-    seconds.style.backgroundColor = "transparent"
+    seconds.disabled = true
+    // seconds.style.backgroundColor = "transparent"
+    seconds.style.border = "none"
     minutes.disabled = true
-    minutes.style.backgroundColor = "transparent"
+    // minutes.style.backgroundColor = "transparent"
+    minutes.style.border = "none"
     hours.disabled = true
-    hours.style.backgroundColor = "transparent"
-    
+    // hours.style.backgroundColor = "transparent"
+    hours.style.border = "none"
     if(!t){
-        t = setInterval(setTimeout(startCountDown, 1000), 1000)
+        t = setInterval(startCountDown, 1000)
     }
 }
 
@@ -19,17 +25,42 @@ let startCountDown = () =>{
         if (seconds.value <= 9){
             seconds.value = '0' + seconds.value 
         }
-    }  
+    }
+    if (seconds.value == 0 && minutes.value > 0){
+        minutes.value--
+        seconds.value = 59
+        if (minutes.value <= 9){
+            minutes.value = '0' + minutes.value 
+        }
     
+    }
+    if (minutes.value == 0 && hours.value > 0){
+        hours.value--
+        seconds.value = 59
+        if (hours.value <= 9){
+            hours.value = '0' + hours.value 
+        }
+    }
+    if (hours.value == 00 && minutes.value == 00 && seconds.value == 00){
+        stop_value.innerHTML += `Countdown completed`
+        clearInterval(t)
+    }    
 }
 
 stopCountDown = () =>{
     clearInterval(t)
-    stop_value.innerHTML += `${seconds.value} : ${minutes.value} : ${minutes.value} | `
+    stop_value.innerHTML += `Countdown completed`
 }
 
-// let reStart = () =>{
-//     seconds.value = '00'
-//     minutes.value = '00'
-//     hours.value = '00'
-// }
+let reStart = () =>{
+    seconds.value = '00'
+    minutes.value = '00'
+    hours.value = '00'
+    console.log(seconds.disabled);
+    seconds.disabled = false
+    seconds.style.backgroundColor = "#7c7c7c"
+    seconds.style.border = "none"
+    minutes.disabled = false
+    hours.disabled = false
+
+}
