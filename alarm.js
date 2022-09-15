002da30b2a502ad20ea3bdf3668e1c0e360d286d
Timimah <timimah0117@gmail.com>
2022-09-15 15:24:40 +0100

    deleted and renamed some files

--- alarm.js
+++
@@ -1,6 +0,0 @@
-getCurrentTime = () =>{
-    let currentTime=new Date();
-    currentTime=currentTime.toLocaleTimeString('en-US');
-    date_display.innerHTML = currentTime
-}
-    setInterval(getCurrentTime, 1000)
